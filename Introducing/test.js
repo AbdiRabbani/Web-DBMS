cae0f131eda047b5665a1835a84cfdc1d93df35b
AbdiRabbani <abdirabbani59@gmail.com>
2022-01-20 11:19:36 +0700

    initial commit

---
+++ Introducing/test.js
@@ -0,0 +1,69 @@
+//!identifier 
+/*
+adalah penamaan variable, fungsi ataupun parameter 
+karakter pertamanya hanya bisa huruf a-z dan simbol Underscrore atau dollar
+tdk boleh menggunakan angka di depan nama variable seperti "var 10rpl"
+tidak boleh menggunakan bahasa yang sudah ada deafult di js
+*/
+
+//!statement
+/*
+suatu pernyataan js yang paling dasar dan memiliki tugas tertentu
+*/
+
+let name = "Abdi" //statement dasar / biasa
+console.log(name)
+
+//!expression
+/*
+merupakan ekspresi js yang  berfungsi untuk menghasilkan nilai tertentu.
+*/
+
+let first = 5
+let seccond = 2
+let result = first + seccond
+console.log(result)
+
+
+if (result >= 5) {
+    console.log("jawaban anda " + result)
+} else if (result <= 5) {
+    console.log("jawaban anda " + result)
+}
+
+//!scope
+/*
+cakupan kode agar bisa mengakses atau membatasi
+*/
+
+var a = "Hello my friend"
+
+function hi() {
+    console.log(a)
+}
+
+function hi2() {
+    a = "nilainya sudah di rubah"
+    console.log(a)
+}
+
+hi()
+hi2()
+
+//const = tidak bisa di rubah
+//var = bisa di ubah ubah dan bersifat global
+//let = bisa di ubah namun lebih ke privat jadi tidak bisa di rubah di scope lain
+
+const car = "toyota"
+
+function kendaraan() {
+    console.log(car)
+    if (true) {
+        const motor = "honda"
+
+        console.log(motor) //bisa di panggil karena satu scope
+    }
+    /*console.log(motor)*/  //tidak bisa karenanmenggunakan const di luar scope
+}
+
+kendaraan()
